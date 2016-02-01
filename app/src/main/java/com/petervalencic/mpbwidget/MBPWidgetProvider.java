f812a90b7c3beb5ffc505a8c60b48ca4d4a2b412
peterv <peter.valencic@gmail.com>
2016-02-01 15:23:14 +0100

    - oblika

--- app/src/main/java/com/petervalencic/mpbwidget/MBPWidgetProvider.java
+++ app/src/main/java/com/petervalencic/mpbwidget/MBPWidgetProvider.java
@@ -1,51 +1,83 @@
 package com.petervalencic.mpbwidget;
 
-import java.util.Random;
-
-import android.app.PendingIntent;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
-import android.widget.RemoteViews;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by peterv on 1.2.2016.
  */
 public class MBPWidgetProvider extends AppWidgetProvider
 {
+    private static final String LOG = "MBPWidget";
     private static final String ACTION_CLICK = "ACTION_CLICK";
+    private static JSONObject jsonObject;
+    private static String json;
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager,
                          int[] appWidgetIds) {
 
+        Log.w(LOG, "onUpdate method called");
         // Get all ids
         ComponentName thisWidget = new ComponentName(context,
                 MBPWidgetProvider.class);
         int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
-        for (int widgetId : allWidgetIds) {
-            // create some random data
-            int number = (new Random().nextInt(100));
+        Log.v(LOG,""+allWidgetIds.length);
 
-            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
-                    R.layout.mbp_layout);
-            Log.w("WidgetExample", String.valueOf(number));
-            // Set the text
-            remoteViews.setTextViewText(R.id.update, String.valueOf(number));
+        // Build the intent to call the service
+        Intent intent = new Intent(context.getApplicationContext(), UpdateWidgetService.class);
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, allWidgetIds);
 
-            // Register an onClickListener
-            Intent intent = new Intent(context, MBPWidgetProvider.class);
+        // Update the widgets via the service
+        context.startService(intent);
+    }
 
-            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+    public JSONObject getJSONFromUrl(String url) {
 
-            PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
-                    0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-            remoteViews.setOnClickPendingIntent(R.id.update, pendingIntent);
-            appWidgetManager.updateAppWidget(widgetId, remoteViews);
+        InputStream is = null;
+        URL naslov = null;
+        // defaultHttpClient
+        try {
+            naslov = new URL(url);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
         }
+
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(naslov.openStream()));
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            reader.close();
+            json = sb.toString();
+        } catch (Exception e) {
+            Log.e("WEB", "Error converting result " + e.toString());
+        }
+
+        // try parse the string to a JSON object
+        try {
+            jsonObject = new JSONObject(json);
+        } catch (JSONException e) {
+            Log.e("JSON Parser", "Error parsing data " + e.toString());
+        }
+
+        // return JSON String
+        return jsonObject;
+
     }
 }
---
+++ app/src/main/java/com/petervalencic/mpbwidget/UpdateWidgetService.java
@@ -0,0 +1,72 @@
+package com.petervalencic.mpbwidget;
+
+/**
+ * Created by peterv on 1.2.2016.
+ */
+import java.util.Random;
+
+import android.app.PendingIntent;
+import android.app.Service;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+public class UpdateWidgetService extends Service {
+    public UpdateWidgetService()
+    {
+        super();
+        Log.w(LOG, "Konstruktor");
+    }
+    private static final String LOG = "MBPWidget";
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        Log.w(LOG, "onStart");
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this
+                .getApplicationContext());
+
+        int[] allWidgetIds = intent
+                .getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS);
+
+//    ComponentName thisWidget = new ComponentName(getApplicationContext(),
+//        MyWidgetProvider.class);
+//    int[] allWidgetIds2 = appWidgetManager.getAppWidgetIds(thisWidget);
+
+        for (int widgetId : allWidgetIds) {
+            // create some random data
+            int number = (new Random().nextInt(100));
+
+            RemoteViews remoteViews = new RemoteViews(this
+                    .getApplicationContext().getPackageName(),
+                    R.layout.mbp_layout);
+            Log.w(LOG, String.valueOf(number));
+            // Set the text
+            remoteViews.setTextViewText(R.id.update,
+                    "Random: " + String.valueOf(number));
+
+            // Register an onClickListener
+            Intent clickIntent = new Intent(this.getApplicationContext(),
+                    MBPWidgetProvider.class);
+
+            clickIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            clickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS,
+                    allWidgetIds);
+
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, clickIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT);
+            remoteViews.setOnClickPendingIntent(R.id.update, pendingIntent);
+            appWidgetManager.updateAppWidget(widgetId, remoteViews);
+        }
+        stopSelf();
+
+        super.onStart(intent, startId);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
