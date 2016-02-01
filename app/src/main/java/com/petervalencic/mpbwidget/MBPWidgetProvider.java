d6c6711da9cb215697f4278a82c5b38c1d22e4bd
Peter <peter.valencic@gmail.com>
2016-02-01 18:27:39 +0100

    - spremembe kode

--- app/src/main/java/com/petervalencic/mpbwidget/MBPWidgetProvider.java
+++ app/src/main/java/com/petervalencic/mpbwidget/MBPWidgetProvider.java
@@ -5,12 +5,19 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.RemoteViews;
+import android.widget.Toast;
 
 
 import org.json.JSONException;
@@ -25,25 +32,53 @@ public class MBPWidgetProvider extends AppWidgetProvider
     private static final String ACTION_CLICK = "ACTION_CLICK";
     private static JSONObject jsonObject;
     private static String json;
+    private static SimpleDateFormat formatter = new SimpleDateFormat("dd MMM yyyy  hh:mm:ss a");
+    static String strWidgetText = "";
+
+
 
     @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
-                         int[] appWidgetIds) {
+    public void onReceive(Context context, Intent intent) {
+        // TODO Auto-generated method stub
+        super.onReceive(context, intent);
+        Log.w(LOG, "onReceive method called");
 
-        Log.w(LOG, "onUpdate method called");
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager,int[] appWidgetIds) {
+
+        Log.w(LOG, "updateAppWidget");
         // Get all ids
         ComponentName thisWidget = new ComponentName(context,
                 MBPWidgetProvider.class);
         int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
-        Log.v(LOG,""+allWidgetIds.length);
+        for (int widgetId : allWidgetIds) {
 
-        // Build the intent to call the service
-        Intent intent = new Intent(context.getApplicationContext(), UpdateWidgetService.class);
-        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, allWidgetIds);
 
-        // Update the widgets via the service
-        context.startService(intent);
+            // create some random data
+            int number = (new Random().nextInt(100));
+
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
+                    R.layout.mbp_layout);
+            Log.w("WidgetExample", String.valueOf(number));
+            // Set the text
+            remoteViews.setTextViewText(R.id.update, String.valueOf(number));
+
+            // Register an onClickListener
+            Intent intent = new Intent(context, MBPWidgetProvider.class);
+
+            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+                    0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            remoteViews.setOnClickPendingIntent(R.id.layout, pendingIntent);
+            appWidgetManager.updateAppWidget(widgetId, remoteViews);
+        }
+
     }
+
 
     public JSONObject getJSONFromUrl(String url) {
 
