692eadec0985ebe84da0d3346f5652cf74f0ff6b
Peter <peter.valencic@gmail.com>
2016-02-03 06:44:42 +0100

    -dodan json parsanje

--- app/src/main/java/com/petervalencic/mpbwidget/MBPWidgetProvider.java
+++ app/src/main/java/com/petervalencic/mpbwidget/MBPWidgetProvider.java
@@ -1,71 +1,57 @@
 package com.petervalencic.mpbwidget;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Random;
-
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.RemoteViews;
-import android.widget.Toast;
-
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Random;
+
 /**
  * Created by peterv on 1.2.2016.
  */
-public class MBPWidgetProvider extends AppWidgetProvider
-{
+public class MBPWidgetProvider extends AppWidgetProvider {
     private static final String LOG = "MBPWidget";
     private static final String ACTION_CLICK = "ACTION_CLICK";
     private static JSONObject jsonObject;
     private static String json;
-    private static SimpleDateFormat formatter = new SimpleDateFormat("dd MMM yyyy  hh:mm:ss a");
-    static String strWidgetText = "";
     static Context context;
     static AppWidgetManager appWidgetManager;
     static int[] appWidgetIds;
-
-
-
 
     @Override
     public void onReceive(Context context, Intent intent) {
         // TODO Auto-generated method stub
         super.onReceive(context, intent);
         Log.w(LOG, "onReceive method called");
+        MyTask task = new MyTask();
+        task.execute("http://www.nib.si/mbp/apps/compass.rose/webapi/CompassRose/getData");
 
         //
 
     }
 
+
     @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager,int[] appWidgetIds) {
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
 
         Log.w(LOG, "updateAppWidget");
-        this.context=context;
-        this.appWidgetIds=appWidgetIds;
-        this.appWidgetManager=appWidgetManager;
-        MyTask task = new MyTask();
-        task.execute("http://www.nib.si/mbp/apps/compass.rose/webapi/CompassRose/getData");
-
+        this.context = context;
+        this.appWidgetIds = appWidgetIds;
+        this.appWidgetManager = appWidgetManager;
 
         // Get all ids
-        ComponentName thisWidget = new ComponentName(context,
-                MBPWidgetProvider.class);
+        ComponentName thisWidget = new ComponentName(context, MBPWidgetProvider.class);
         int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
         for (int widgetId : allWidgetIds) {
 
@@ -73,41 +59,57 @@ public class MBPWidgetProvider extends AppWidgetProvider
             // create some random data
             int number = (new Random().nextInt(100));
 
-            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
-                    R.layout.mbp_layout);
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.mbp_layout);
+
             Log.w("WidgetExample", String.valueOf(number));
-            // Set the text
-            remoteViews.setTextViewText(R.id.update, String.valueOf(number));
 
             // Register an onClickListener
             Intent intent = new Intent(context, MBPWidgetProvider.class);
-
             intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
             intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
 
-            PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
-                    0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-            remoteViews.setOnClickPendingIntent(R.id.layout, pendingIntent);
-            appWidgetManager.updateAppWidget(widgetId, remoteViews);
-        }
 
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            remoteViews.setOnClickPendingIntent(R.id.layout, pendingIntent);
+
+            appWidgetManager.updateAppWidget(widgetId, remoteViews);
+            // showWebPage(context, appWidgetManager, widgetId);
+
+
+        }
+        //MyTask task = new MyTask();
+        //task.execute("http://www.nib.si/mbp/apps/compass.rose/webapi/CompassRose/getData");
 
 
     }
 
-    public static void osveziPodatke(String jsonPodatki)
-    {
+    public static void showWebPage(Context ctx, AppWidgetManager appWidgetManager, int widgetID) {
+        RemoteViews widgetView = new RemoteViews(ctx.getPackageName(), R.layout.mbp_layout);
+        Uri uri = Uri.parse("http://google.com/");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        PendingIntent pIntent = PendingIntent.getActivity(ctx, widgetID, intent, 0);
+        // viewID - our clickable view ID
+        widgetView.setOnClickPendingIntent(R.id.imageView, pIntent);
+
+        appWidgetManager.updateAppWidget(widgetID, widgetView);
+
+
+    }
+
+    public static void osveziPodatke(String jsonPodatki) {
         String currentTime;
-        double currentWindSpeed = 0;
-        double currentWindDirection = 0;
-        double meanWindSpeed = 0;
-        double meanWindBeaufort = 0;
-        double meanWindDirection = 0;
-        double temperatureSeaSurface = 0;
-        double temperatureAir = 0;
-        double wavesHeight = 0;
-        double wavesDirection = 0;
-        double wavesPeriod = 0;
+        double currentWindSpeed;
+        double currentWindDirection;
+        double meanWindSpeed;
+        double meanWindBeaufort;
+        double meanWindDirection;
+        double temperatureSeaSurface;
+        double temperatureAir;
+        double wavesHeight;
+        double wavesDirection;
+        double wavesPeriod;
 
         Log.w(LOG, jsonPodatki);
         try {
@@ -124,25 +126,43 @@ public class MBPWidgetProvider extends AppWidgetProvider
             wavesDirection = jsonObject.getDouble("wavesDirection");
             wavesPeriod = jsonObject.getDouble("wavesPeriod");
 
-            Log.d(LOG,"##>" + jsonObject.getString("currentTime"));
-            Log.d(LOG,"##>" + currentTime);
-            Log.d(LOG,"##>" + currentWindSpeed);
-            Log.d(LOG,"##>" + currentWindDirection);
-            Log.d(LOG,"##>" + meanWindSpeed);
-            Log.d(LOG,"##>" + meanWindBeaufort);
-            Log.d(LOG,"##>" + meanWindDirection);
-            Log.d(LOG,"##>" + temperatureSeaSurface);
-            Log.d(LOG,"##>" + temperatureAir);
-            Log.d(LOG,"##>" + wavesHeight);
-            Log.d(LOG,"##>" + wavesDirection);
+            Log.d(LOG, "##>" + jsonObject.getString("currentTime"));
+            Log.d(LOG, "##>" + currentTime);
+            Log.d(LOG, "##>" + currentWindSpeed);
+            Log.d(LOG, "##>" + currentWindDirection);
+            Log.d(LOG, "##>" + meanWindSpeed);
+            Log.d(LOG, "##>" + meanWindBeaufort);
+            Log.d(LOG, "##>" + meanWindDirection);
+            Log.d(LOG, "##>" + temperatureSeaSurface);
+            Log.d(LOG, "##>" + temperatureAir);
+            Log.d(LOG, "##>" + wavesHeight);
+            Log.d(LOG, "##>" + wavesDirection);
             Log.d(LOG, "##>" + wavesPeriod);
 
-            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),R.layout.mbp_layout);
-            remoteViews.setTextViewText(R.id.textView, String.valueOf(currentWindSpeed));
+            //osvežimo zapis na widget-u
+            ComponentName thisWidget = new ComponentName(context, MBPWidgetProvider.class);
 
-        }catch (JSONException ex)
-        {
-            Log.e(LOG,"Napaka pri parsanju JSON");
+            int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
+            for (int widgetId : allWidgetIds) {
+                Resources res = context.getResources();
+                RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.mbp_layout);
+                if (res != null) {
+                    String text = String.format(res.getString(R.string.s_currentWindSpeed), "123");
+                    Log.e(LOG, text);
+                    remoteViews.setTextViewText(R.id.t_currentWindSpeed, String.format(res.getString(R.string.s_currentWindSpeed), currentWindSpeed));
+                    remoteViews.setTextViewText(R.id.t_currentWindDirection, String.format(res.getString(R.string.s_currentWindDirection), currentWindDirection));
+                    remoteViews.setTextViewText(R.id.t_meanWindSpeed, String.format(res.getString(R.string.s_meanWindSpeed), meanWindSpeed));
+                    remoteViews.setTextViewText(R.id.t_meanWindDirection, String.format(res.getString(R.string.s_meanWindDirection), meanWindDirection));
+                    remoteViews.setTextViewText(R.id.t_meanWindBeaufort, String.format(res.getString(R.string.s_meanWindBeaufort), meanWindBeaufort));
+                    remoteViews.setTextViewText(R.id.t_temperatureSeaSurface, String.format(res.getString(R.string.s_temperatureSeaSurface), temperatureSeaSurface));
+                    remoteViews.setTextViewText(R.id.t_temperatureAir, String.format(res.getString(R.string.s_temperatureAir), temperatureAir));
+
+                }
+                appWidgetManager.updateAppWidget(widgetId, remoteViews);
+            }
+
+        } catch (JSONException ex) {
+            Log.e(LOG, "Napaka pri parsanju JSON");
         }
 
     }
@@ -157,16 +177,14 @@ public class MBPWidgetProvider extends AppWidgetProvider
 
         @Override
         protected String doInBackground(String... params) {
-
-            String content = HttpManager.getData(params[0]);
-            return content;
+            Log.w("MBPWidget", "poklican AsyncTask" + params[0]);
+            return HttpManager.getData(params[0]);
         }
 
         protected void onPostExecute(String result) {
-           if (result != null)
-           {
-              osveziPodatke(result);
-           }
+            if (result != null) {
+                osveziPodatke(result);
+            }
         }
 
     }
